a0acd1d081c5f692b669a1885ec8ee5cb063c302
AS-DEVELOPER <67372463+AS-DEVELOPER7@users.noreply.github.com>
2022-07-06 14:03:47 +0530

    Update Photo.jsx

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -4,9 +4,9 @@ import {DownloadIcon, ShareIcon}from '@heroicons/react/solid'
 import { WhatsappShareButton } from 'react-share'
 const Photo = ({download,img}) => {
   return (
-    <div className='relative rounded-xl shadow-md overflow-hidden'>
-      <img src={img} className=''/>
-      <div className='absolute top-2 right-2 flex space-x-6 lg:space-x-3'>
+    <div className='relative rounded-xl shadow-md overflow-hidden group  z-0'>
+      <img src={img} className='filter hover:brightness-75' />
+      <div className='absolute opacity-0 top-2 right-2 flex space-x-6 lg:space-x-3 group-hover:opacity-100'>
 
 <WhatsappShareButton url={download} className='cursor-pointer'>
   <ShareIcon className='text-slate-900 bg-slate-200 bg-opacity-70  lg:h-[2.5em] p-2 rounded-lg'/>
