a45b1fc9700b120de2cd2e0503dfa3544074d93f
AS-DEVELOPER <67372463+AS-DEVELOPER7@users.noreply.github.com>
2022-07-06 14:03:03 +0530

    Update Gallery.jsx

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -1,21 +1,29 @@
 
 import { useStateContext } from '../context/StateContext'
 import Photo from './Photo'
+import Pagination from './Pagination'
+import { BeatLoader } from 'react-spinners'
 const Gallery = () => {
     const{isLoading,images}=useStateContext()
    
   return (
-    <div className='w-[95%] mx-auto columns-2 lg:columns-3 mt-[2em] space-y-4 lg:mr-[1.5em] '>
+    <div className="">
+
+    <div className='w-[95%] mx-auto columns-2 lg:columns-3 mt-[2em] space-y-4 lg:mr-[1.5em] z-0'>
       {isLoading ? (
           <div className="flex justify-center items-center">
             <BeatLoader color="#242424" size={150} />
           </div>
         ) :  (<>
        {images?.map((photo)=>(
-            <Photo key={photo.id} download={photo.links.download} img={photo.urls.regular}/>
-        ))}
+         <Photo key={photo.id} download={photo.links.download} img={photo.urls.regular}/>
+         ))}
         </>)}
     </div>
+        <div className="w-[90%] mx-auto">
+          <Pagination/>
+        </div>
+         </div>
   )
 }
 
