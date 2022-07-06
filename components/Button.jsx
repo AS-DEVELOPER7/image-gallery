93961d6f79d0a14c37c21290508281be62501769
AS-DEVELOPER <67372463+AS-DEVELOPER7@users.noreply.github.com>
2022-07-06 14:02:20 +0530

    Update Button.jsx

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { useStateContext } from '../context/StateContext'
 const Button = ({name,types}) => {
-    const {type,setType}=useStateContext()
+    const {type,setType,setPage}=useStateContext()
   return (
-    <button className={`${type===types?'border-b-[3px] text-slate-900 border-black':''} font-semibold text-slate-500 mt-6 tracking-widest`} onClick={()=>setType(types)}>{name}</button>
+    <button className={`${type===types?'border-b-[3px] text-slate-900 border-black':''} font-semibold text-slate-500 mt-6 tracking-widest`} onClick={()=>{
+      setType(types)
+      setPage(1)
+    }}>{name}</button>
   )
 }
 
