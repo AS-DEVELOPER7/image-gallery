d311bca1f402619c751c4856e1e58ea8b693dfbf
AS-DEVELOPER <67372463+AS-DEVELOPER7@users.noreply.github.com>
2022-07-06 14:06:10 +0530

    Update services.js

--- app/services.js
+++ app/services.js
@@ -4,17 +4,16 @@ export const photoGallery = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "https://api.unsplash.com/",
   }),
-
   endpoints: (builder) => ({
     getAllImages: builder.query({
-      query: () => ({
-        url: "photos?page=1&per_page=30&client_id=kf_cmNioNgIVPYizoIyCvhh5x41uEJGedZgIihK5bng",
+      query: (page = 1) => ({
+        url: `photos?page=${page}&per_page=30&client_id=kf_cmNioNgIVPYizoIyCvhh5x41uEJGedZgIihK5bng`,
         method: "GET",
       }),
     }),
     getSearchedImages: builder.query({
-      query: (type) => ({
-        url: `search/photos?client_id=kf_cmNioNgIVPYizoIyCvhh5x41uEJGedZgIihK5bng&page=1&per_page=30&query=${type}`,
+      query: (type, page = 1) => ({
+        url: `search/photos?page=${page}&per_page=30&query=${type}&client_id=kf_cmNioNgIVPYizoIyCvhh5x41uEJGedZgIihK5bng`,
         method: "GET",
       }),
     }),
